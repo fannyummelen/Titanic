a06bd7c4aa479632d2c75418dcd1f37c6b95c8cb
Fanny Ummelen <fannyummelen@hotmail.com>
2020-04-12 15:50:29 +0200

    Extra comment for loading modules added

--- titanic.py
+++ titanic.py
@@ -7,6 +7,7 @@
 #															  #	
 ###############################################################
 
+# Load python modules
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
